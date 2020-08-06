871d698f51157dd46717c9f6b498a136db181daa
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-05 23:51:42 -0300

    test: ensure SignUpController return an error when email is not provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -16,4 +16,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Field name is required'));
   });
+
+  it('should return 400 if email was not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'anyname',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Field e-mail is required'));
+  });
 });
