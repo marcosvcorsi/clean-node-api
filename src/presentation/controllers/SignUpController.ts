2e242996cbdbbcf3a9bde5cde5c8c5216d532013
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-05 23:50:45 -0300

    feat: ensure SignUpController return and error with email is not provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,10 +1,19 @@
 export default class SignUpController {
   handle(request: any): any {
-    console.log(request);
+    if (!request.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Field name is required'),
+      };
+    }
 
-    return {
-      statusCode: 400,
-      body: new Error('Field name is required'),
-    };
+    if (!request.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Field e-mail is required'),
+      };
+    }
+
+    return {};
   }
 }
