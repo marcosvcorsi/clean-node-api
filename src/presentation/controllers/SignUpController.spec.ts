32e6719501c8da9240282c5382b904057ee27e2f
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-05 23:30:13 -0300

    test: ensure signup SignUpController returns 400 if name was not provide

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,18 @@
+import SignUpController from './SignUpController';
+
+describe('SignUp Controller', () => {
+  it('should return 400 if name was not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'anymail@mail.com',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
