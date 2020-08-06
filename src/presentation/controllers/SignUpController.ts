a1df501fb1c1115cfdc5953797377eb887a5782f
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-05 23:31:47 -0300

    chore: remove husky from package.json

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,7 +1,10 @@
 export default class SignUpController {
   handle(request: any): any {
+    console.log(request);
+
     return {
       statusCode: 400,
+      body: new Error('Field name is required'),
     };
   }
 }
