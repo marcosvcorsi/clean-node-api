dd4b6a6a537d6af479ce92078434b8d03265b731
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-06 23:35:19 -0300

    test: ensure SignUpController returns an error with no password confirmation was provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -2,7 +2,7 @@ import SignUpController from './SignUpController';
 import MissingParamError from '../errors/MissingParamError';
 
 describe('SignUp Controller', () => {
-  it('should return 400 if name was not provided', () => {
+  it('should return an error if name was not provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -18,7 +18,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 
-  it('should return 400 if email was not provided', () => {
+  it('should return an error if email was not provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -34,7 +34,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 
-  it('should return 400 if password was not provided', () => {
+  it('should return an error if password was not provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -48,5 +48,23 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
+
+  it('should return an error if password confirmation was not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'anyname',
+        email: 'anymail@mail.com',
+        password: 'anypassword',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError('passwordConfirmation'),
+    );
   });
 });
