59ccf8948f846db71c90dfdc7b9637f86a11605e
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-06 23:32:25 -0300

    feat: ensure SignUpController returns error with no password was provider

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/httpHelper';
 
 export default class SignUpController {
   handle(request: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email'];
+    const requiredFields = ['name', 'email', 'password'];
 
     for (const requiredField of requiredFields) {
       if (!request.body[requiredField]) {
