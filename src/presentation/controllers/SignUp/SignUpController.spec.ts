e8162ac91190174412464de112951dce4d03621e
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-09 23:48:13 -0300

    test: change SignController to return 201 when create

--- src/presentation/controllers/SignUp/SignUpController.spec.ts
+++ src/presentation/controllers/SignUp/SignUpController.spec.ts
@@ -269,7 +269,7 @@ describe('SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest);
 
-    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.statusCode).toBe(201);
     expect(httpResponse.body).toEqual({
       id: 'valid_id',
       name: 'valid_name',
