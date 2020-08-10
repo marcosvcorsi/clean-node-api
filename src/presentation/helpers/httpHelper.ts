4eeb5be59061d20b1c9a0afd405854274b42dffb
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-10 00:04:07 -0300

    refactor: change import http protocol to default

--- src/presentation/helpers/httpHelper.ts
+++ src/presentation/helpers/httpHelper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '../protocols/http';
+import { HttpResponse } from '../protocols';
 import { ServerError } from '../errors';
 
 export const badRequest = (error: Error): HttpResponse => ({
