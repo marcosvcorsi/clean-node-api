8110c74ae159630687a54e172fd85f5f27a76db3
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-10 20:36:37 -0300

    chore: add jest config and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.test.ts'];
+
+module.exports = config;
---
+++ jest-unit-config.js
@@ -0,0 +1,5 @@
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.spec.ts'];
+
+module.exports = config;
