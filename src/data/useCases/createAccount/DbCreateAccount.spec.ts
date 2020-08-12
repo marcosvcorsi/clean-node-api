99baae7e644e7b9681122a4b80c89f6bf3426abb
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-11 21:35:09 -0300

    test: ensure DbCreateAccount throws an Error

--- src/data/useCases/createAccount/DbCreateAccount.spec.ts
+++ src/data/useCases/createAccount/DbCreateAccount.spec.ts
@@ -40,4 +40,20 @@ describe('DbCreateAccount Use Case', () => {
 
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
   });
+
+  it('should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    };
+
+    await expect(sut.create(accountData)).rejects.toThrow();
+  });
 });
