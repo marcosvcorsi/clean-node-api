6a2ee204d741231ae3a75da7a1b53a850b037f70
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-15 21:04:46 -0300

    feat: ensure LogControllerDecorate call handle

--- src/factories/signup.ts
+++ src/factories/signup.ts
@@ -3,8 +3,10 @@ import EmailValidatorAdapter from '../utils/EmailValidatorAdapter';
 import DbCreateAccount from '../data/useCases/createAccount/DbCreateAccount';
 import { BcryptAdapter } from '../infra/cryptography/BcryptAdapter';
 import { AccountMongoRepository } from '../infra/db/mongodb/repositories/account/AccountRepository';
+import { LogControllerDecorator } from '../main/decorators/LogController';
+import { Controller } from '../presentation/protocols';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter();
 
   const salt = 12;
@@ -17,5 +19,7 @@ export const makeSignUpController = (): SignUpController => {
     accountMongoRepository,
   );
 
-  return new SignUpController(emailValidator, createAccount);
+  const signUpController = new SignUpController(emailValidator, createAccount);
+
+  return new LogControllerDecorator(signUpController);
 };
---
+++ src/main/decorators/LogController.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '../../presentation/protocols';
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller;
+
+  constructor(controller: Controller) {
+    this.controller = controller;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest);
+
+    if (httpResponse.statusCode === 500) {
+      // handle error
+    }
+
+    return httpResponse;
+  }
+}
