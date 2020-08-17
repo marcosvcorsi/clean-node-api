0481a4c0e03c3120b6ed7253cdee47056e20cba1
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-17 20:43:55 -0300

    test: ensure LoginController validate password required

--- src/presentation/controllers/login/LoginController.spec.ts
+++ src/presentation/controllers/login/LoginController.spec.ts
@@ -2,9 +2,13 @@ import { LoginController } from './LoginController';
 import { badRequest } from '../../helpers/httpHelper';
 import { MissingParamError } from '../../errors';
 
+const makeSut = (): LoginController => {
+  return new LoginController();
+};
+
 describe('LoginController', () => {
   it('should return an error with no email was provided', async () => {
-    const sut = new LoginController();
+    const sut = makeSut();
 
     const httpRequest = {
       body: {
@@ -15,5 +19,19 @@ describe('LoginController', () => {
     const httpReponse = await sut.handle(httpRequest);
 
     expect(httpReponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+
+  it('should return an error with no password was provided', async () => {
+    const sut = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: 'anymail@mail.com',
+      },
+    };
+
+    const httpReponse = await sut.handle(httpRequest);
+
+    expect(httpReponse).toEqual(badRequest(new MissingParamError('password')));
   });
 });
