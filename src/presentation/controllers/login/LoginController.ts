a757df82aa8e2c4d4a9f7e38d27840cde26c4139
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-18 20:40:56 -0300

    feat: ensure LoginController authenticate credentials

--- src/presentation/controllers/login/LoginController.ts
+++ src/presentation/controllers/login/LoginController.ts
@@ -46,7 +46,7 @@ export class LoginController implements Controller {
         return unauthorized();
       }
 
-      return ok({});
+      return ok({ accessToken });
     } catch (error) {
       return serverError(error);
     }
