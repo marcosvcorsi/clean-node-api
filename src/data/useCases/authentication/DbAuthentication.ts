e938c468e05279da0409a1ee541e262619ade578
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-23 20:49:44 -0300

    feat: ensure DbAuthentication call TokenGenerator

--- src/data/useCases/authentication/DbAuthentication.ts
+++ src/data/useCases/authentication/DbAuthentication.ts
@@ -4,18 +4,23 @@ import {
 } from '../../../domain/useCases/Authentication';
 import { LoadAccountByEmailRepository } from '../../protocols/db/LoadAccountByEmailRepository';
 import { HashComparer } from '../../protocols/cryptography/HashComparer';
+import { TokenGenerator } from '../../protocols/cryptography/TokenGenerator';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
 
   private readonly hashComparer: HashComparer;
 
+  private readonly tokenGenerator: TokenGenerator;
+
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashComparer = hashComparer;
+    this.tokenGenerator = tokenGenerator;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string> {
@@ -28,6 +33,8 @@ export class DbAuthentication implements Authentication {
         authentication.password,
         account.password,
       );
+
+      await this.tokenGenerator.generate(account.id);
     }
 
     return null;
