2cfa28c2c16c644b610fe84fd1ff8555ba6f8a92
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-23 21:14:20 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository

--- src/data/useCases/authentication/DbAuthentication.ts
+++ src/data/useCases/authentication/DbAuthentication.ts
@@ -5,6 +5,7 @@ import {
 import { LoadAccountByEmailRepository } from '../../protocols/db/LoadAccountByEmailRepository';
 import { HashComparer } from '../../protocols/cryptography/HashComparer';
 import { TokenGenerator } from '../../protocols/cryptography/TokenGenerator';
+import { UpdateAccessTokenRepository } from '../../protocols/db/UpdateAccessTokenRepository';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
@@ -13,14 +14,18 @@ export class DbAuthentication implements Authentication {
 
   private readonly tokenGenerator: TokenGenerator;
 
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository;
+
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
     tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashComparer = hashComparer;
     this.tokenGenerator = tokenGenerator;
+    this.updateAccessTokenRepository = updateAccessTokenRepository;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string> {
@@ -42,6 +47,8 @@ export class DbAuthentication implements Authentication {
     }
 
     const accessToken = await this.tokenGenerator.generate(account.id);
+
+    await this.updateAccessTokenRepository.update(account.id, accessToken);
 
     return accessToken;
   }
