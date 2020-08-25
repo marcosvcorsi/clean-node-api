6d38c8b788bb56b5031d474a691e62a53178e8ac
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-24 21:02:00 -0300

    test: ensure JwtAdapter return token from sign

--- src/infra/cryptography/jwtAdapter/JwtAdapter.spec.ts
+++ src/infra/cryptography/jwtAdapter/JwtAdapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './JwtAdapter';
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return Promise.resolve('any_token');
+  },
+}));
+
 describe('Jwt Adapter', () => {
   it('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret');
@@ -10,5 +16,13 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id');
 
     expect(jwtSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  it('should return a token from sign with correct values', async () => {
+    const sut = new JwtAdapter('secret');
+
+    const token = await sut.encrypt('any_id');
+
+    expect(token).toBe('any_token');
   });
 });
