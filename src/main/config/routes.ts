910b0a49e9b9959f41bfbae7e7077fe8d1b18671
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-28 20:18:25 -0300

    refactor: change routes to use fs and scripts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,17 @@
 import { Express, Router } from 'express';
-import fg from 'fast-glob';
+import { readdirSync } from 'fs';
+import path from 'path';
 
 export default (app: Express): void => {
   const router = Router();
 
   app.use('/api', router);
 
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-    (await import(`../../../${file}`)).default(router);
-  });
+  const routesPath = path.resolve(__dirname, '..', 'routes');
+
+  readdirSync(routesPath)
+    .filter(file => !file.includes('.test.'))
+    .map(async file => {
+      (await import(path.resolve(routesPath, file))).default(router);
+    });
 };
