15af42ce8bdb715eee2ad153a6cd05cd26b7ece7
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-31 23:02:44 -0300

    feat: ensure DbCreateAccount load by email

--- src/data/useCases/createAccount/DbCreateAccount.ts
+++ src/data/useCases/createAccount/DbCreateAccount.ts
@@ -4,15 +4,19 @@ import {
   CreateAccountModel,
   AccountModel,
   CreateAccountRepository,
+  LoadAccountByEmailRepository,
 } from './DbCreateAccountProtocols';
 
 export default class DbCreateAccount implements CreateAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly createAccountRepository: CreateAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {}
 
   async create(accountData: CreateAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
+
     const hashedPassword = await this.hasher.hash(accountData.password);
 
     const account = await this.createAccountRepository.create({
--- src/data/useCases/createAccount/DbCreateAccountProtocols.ts
+++ src/data/useCases/createAccount/DbCreateAccountProtocols.ts
@@ -2,3 +2,4 @@ export * from '../../../domain/useCases/CreateAccount';
 export * from '../../../domain/models/Account';
 export * from '../../protocols/cryptography/Hasher';
 export * from '../../protocols/db/account/CreateAccountRepository';
+export * from '../../protocols/db/account/LoadAccountByEmailRepository';
