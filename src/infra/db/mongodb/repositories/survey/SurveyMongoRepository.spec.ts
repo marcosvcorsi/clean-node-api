ab83b1ca828d7df49d8d8302412a1a71ac966bde
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-03 22:21:24 -0300

    test: ensure SurveyMongoRepository create

---
+++ src/infra/db/mongodb/repositories/survey/SurveyMongoRepository.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb';
+import { MongoHelper } from '../../helpers/mongoHelper';
+import { SurveyMongoRepository } from './SurveyMongoRepository';
+
+let surveyCollection: Collection;
+
+describe('Survey MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+
+    await surveyCollection.deleteMany({});
+  });
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository();
+  };
+
+  it('should insert a new survey', async () => {
+    const sut = makeSut();
+
+    await sut.create({
+      question: 'anyquestion',
+      answers: [
+        { answer: 'anyanswer', image: 'anyimage' },
+        { answer: 'otheranswer' },
+      ],
+    });
+
+    const survey = await surveyCollection.findOne({ question: 'anyquestion' });
+
+    expect(survey).toBeTruthy();
+  });
+});
