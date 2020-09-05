0fbf49411dbc35ca87a3efae3d459722e7b29f4c
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-04 22:15:09 -0300

    feat: ensure AuthMiddleware return server error when throws

--- src/presentation/middlewares/authMiddleware.ts
+++ src/presentation/middlewares/authMiddleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Middleware } from '../protocols';
-import { forbidden, ok } from '../helpers/http/httpHelper';
+import { forbidden, ok, serverError } from '../helpers/http/httpHelper';
 import { AccessDeniedError } from '../errors';
 import { LoadAccountByToken } from '../../domain/useCases/LoadAccountByToken';
 
@@ -7,16 +7,20 @@ export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['Authorization'];
+    try {
+      const accessToken = httpRequest.headers?.['Authorization'];
 
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken);
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken);
 
-      if (account) {
-        return ok({ account_id: account.id });
+        if (account) {
+          return ok({ account_id: account.id });
+        }
       }
-    }
 
-    return forbidden(new AccessDeniedError());
+      return forbidden(new AccessDeniedError());
+    } catch (err) {
+      return serverError(err);
+    }
   }
 }
