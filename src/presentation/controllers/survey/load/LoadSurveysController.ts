4704fb39b577cb0838bac37174ab85a104b54273
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-07 21:39:47 -0300

    feat: ensure LoadSurveysController return survey list

--- src/presentation/controllers/survey/load/LoadSurveysController.ts
+++ src/presentation/controllers/survey/load/LoadSurveysController.ts
@@ -1,18 +1,16 @@
 import {
   Controller,
-  HttpRequest,
   HttpResponse,
   LoadSurveys,
 } from './LoadSurveysControllerProtocols';
+import { ok } from '../../../helpers/http/httpHelper';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    console.log(httpRequest);
+  async handle(): Promise<HttpResponse> {
+    const surveys = await this.loadSurveys.load();
 
-    await this.loadSurveys.load();
-
-    return Promise.resolve(null);
+    return ok(surveys);
   }
 }
