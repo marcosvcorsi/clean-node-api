17ff511e064e0999318472ce81932e6fa52f19cd
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-07 21:45:43 -0300

    feat: ensure LoadSurveyController return serverError

--- src/presentation/controllers/survey/load/LoadSurveysController.ts
+++ src/presentation/controllers/survey/load/LoadSurveysController.ts
@@ -3,14 +3,18 @@ import {
   HttpResponse,
   LoadSurveys,
 } from './LoadSurveysControllerProtocols';
-import { ok } from '../../../helpers/http/httpHelper';
+import { ok, serverError } from '../../../helpers/http/httpHelper';
 
 export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
   async handle(): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load();
+    try {
+      const surveys = await this.loadSurveys.load();
 
-    return ok(surveys);
+      return ok(surveys);
+    } catch (err) {
+      return serverError(err);
+    }
   }
 }
