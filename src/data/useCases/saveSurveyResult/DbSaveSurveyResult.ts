4e2bfe8926f6ebbde4c6e38cd59c8c8c5a70e835
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-10 23:17:00 -0300

    feat: ensure DbSaveSurveyResult return on success

--- src/data/useCases/saveSurveyResult/DbSaveSurveyResult.ts
+++ src/data/useCases/saveSurveyResult/DbSaveSurveyResult.ts
@@ -11,8 +11,8 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   ) {}
 
   async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data);
+    const surveyResult = await this.saveSurveyResultRepository.save(data);
 
-    return null;
+    return surveyResult;
   }
 }
