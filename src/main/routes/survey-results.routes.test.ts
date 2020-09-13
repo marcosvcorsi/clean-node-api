945564a75edd85808f3883c099ea027adf122ad5
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-13 13:27:45 -0300

    test: ensure save survey result return result

--- src/main/routes/survey-results.routes.test.ts
+++ src/main/routes/survey-results.routes.test.ts
@@ -1,7 +1,38 @@
 import request from 'supertest';
+import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
 import app from '../config/app';
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongoHelper';
+
+import authConfig from '../config/auth';
+
+let surveyCollection: Collection;
+let accountCollection: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'anyname',
+    email: 'anymail@mail.com.br',
+    password: 'anypassword',
+    role: 'admin',
+  });
+
+  const [{ _id: id }] = res.ops;
+
+  const accessToken = await sign({ id }, authConfig.secret);
+
+  await accountCollection.updateOne(
+    { _id: id },
+    {
+      $set: {
+        accessToken,
+      },
+    },
+  );
+
+  return accessToken;
+};
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -12,7 +43,15 @@ describe('Survey Result Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  describe('POST /surveys/:surveyId/results', () => {
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    accountCollection = await MongoHelper.getCollection('accounts');
+
+    await surveyCollection.deleteMany({});
+    await accountCollection.deleteMany({});
+  });
+
+  describe('PUT /surveys/:surveyId/results', () => {
     it('should not save survey result on put without authorization', async () => {
       await request(app)
         .put('/api/surveys/anyid/results')
@@ -20,6 +59,29 @@ describe('Survey Result Routes', () => {
           answer: 'anyanswer',
         })
         .expect(403);
+    });
+
+    it('should save survey result on put with authorization', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'Question 1',
+        answers: [
+          { image: 'http://image.com.br/image.jpg', answer: 'Answer 1' },
+          { answer: 'Answer 2' },
+        ],
+        date: new Date(),
+      });
+
+      const [{ _id: surveyId }] = res.ops;
+
+      const accessToken = await makeAccessToken();
+
+      await request(app)
+        .put(`/api/surveys/${surveyId}/results`)
+        .set('Authorization', accessToken)
+        .send({
+          answer: 'Answer 1',
+        })
+        .expect(200);
     });
   });
 });
