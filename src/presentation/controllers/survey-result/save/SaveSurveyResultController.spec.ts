84955e8fbd84b1e42d7e597c4426d853dc71df74
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-13 00:21:33 -0300

    test: ensure SaveSurveyResultController return serverError if throws

--- src/presentation/controllers/survey-result/save/SaveSurveyResultController.spec.ts
+++ src/presentation/controllers/survey-result/save/SaveSurveyResultController.spec.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '@/presentation/helpers/http/httpHelper';
+import { forbidden, serverError } from '@/presentation/helpers/http/httpHelper';
 import { InvalidParamError } from '@/presentation/errors';
 import {
   SurveyModel,
@@ -70,5 +70,17 @@ describe('SaveSurveyController Test', () => {
     const response = await sut.handle(makeFakeRequest());
 
     expect(response).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  it('should return serverError if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+
+    jest
+      .spyOn(loadSurveyByIdStub, 'loadById')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const response = await sut.handle(makeFakeRequest());
+
+    expect(response).toEqual(serverError(new Error()));
   });
 });
