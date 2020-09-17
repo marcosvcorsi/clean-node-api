2b765f7c0b03699a8809f13b613da27bea5b0d44
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-16 22:52:15 -0300

    docs: add login route

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login';
+import { accountSchema } from './schemas/account';
+import { loginParamsSchema } from './schemas/loginParams';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do para realizar enquente entre programadores',
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
---
+++ src/main/docs/paths/login.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'Rota de autenticação de usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams',
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account',
+            },
+          },
+        },
+      },
+    },
+  },
+};
