3e5541060c87882ca3846fa933dd4ba6ccfd5e56
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-18 22:23:02 -0300

    feat: ensure DbLoadSurveyResult return result

--- src/data/useCases/survey-result/loadSurveyResult/DbLoadSurveyResult.ts
+++ src/data/useCases/survey-result/loadSurveyResult/DbLoadSurveyResult.ts
@@ -8,8 +8,10 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
   ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel> {
-    await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
+      surveyId,
+    );
 
-    return null;
+    return surveyResult;
   }
 }
