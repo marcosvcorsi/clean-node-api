c25dc3d0156ba9d008be2762654421d377654c3a
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-20 15:11:26 -0300

    docs: add load survey result route

--- src/main/docs/paths/surveyResult.ts
+++ src/main/docs/paths/surveyResult.ts
@@ -48,4 +48,45 @@ export const surveyResultPath = {
       },
     },
   },
+
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ['Enquetes'],
+    summary: 'Rota para consultar o resultado de uma enquete',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };
