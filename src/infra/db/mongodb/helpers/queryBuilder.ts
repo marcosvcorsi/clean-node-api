9fd075f40426e3e3170451e989ccf111003448dc
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-20 15:16:14 -0300

    refactor: remove unecessary addFields from query builder

--- src/infra/db/mongodb/helpers/queryBuilder.ts
+++ src/infra/db/mongodb/helpers/queryBuilder.ts
@@ -33,14 +33,6 @@ export class QueryBuilder {
     return this;
   }
 
-  addFields(data: object): QueryBuilder {
-    this.query.push({
-      $addFields: data,
-    });
-
-    return this;
-  }
-
   project(data: object): QueryBuilder {
     this.query.push({
       $project: data,
