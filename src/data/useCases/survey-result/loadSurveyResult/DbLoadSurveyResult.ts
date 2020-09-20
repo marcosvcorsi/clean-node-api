078f278130458feb96ae5f14ab6c2c51377681e3
Marcos Corsi <marcosvcorsi@gmail.com>
2020-09-19 22:14:29 -0300

    feat: ensure DbLoadSurveyResult call load survey by id repository if not result exists

--- src/data/useCases/survey-result/loadSurveyResult/DbLoadSurveyResult.ts
+++ src/data/useCases/survey-result/loadSurveyResult/DbLoadSurveyResult.ts
@@ -1,5 +1,6 @@
 import {
   LoadSurveyResultRepository,
+  LoadSurveyByIdRepository,
   LoadSurveyResult,
   SurveyResultModel,
 } from './DbLoadSurveyResultProtocols';
@@ -7,12 +8,17 @@ import {
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor(
     private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository,
   ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
       surveyId,
     );
+
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId);
+    }
 
     return surveyResult;
   }
